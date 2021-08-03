ab1071937374a61a6704045678ab9f03a641a41c
Ala Ho <alaandho@gmail.com>
2021-08-02 20:55:04 -0400

    In class flask day-1

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__)) 
+
+# Give's access to the project in ANY OS we find oursleves in
+# Allows outside files /folder to be added to the project from the base directory
