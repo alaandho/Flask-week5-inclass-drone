baa360541a68e0905ca7e6b83bf5233405cbc72e
Ala Ho <alaandho@gmail.com>
2021-08-04 03:52:48 -0400

    In Class Sign Up & DB

--- config.py
+++ config.py
@@ -4,3 +4,13 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Give's access to the project in ANY OS we find oursleves in
 # Allows outside files /folder to be added to the project from the base directory
+
+
+class Config:
+    """
+    Sets configuration variables for our Flask app here
+    Eventually will use hidden variable items - but for now, we'll leave them exposed in config
+    """
+    SECRET_KEY= "You will never guess..."
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False #Decreases unnecessary output in terminal
