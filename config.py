ac1adbf838f6454c0b7dd2fda3752f3a38e4dfff
Ala Ho <alaandho@gmail.com>
2021-08-06 12:52:02 -0400

    change db

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ class Config:
     Eventually will use hidden variable items - but for now, we'll leave them exposed in config
     """
     SECRET_KEY= "You will never guess..."
-    DEPLOY_DATABASE_URL = os.environ.get('DEPLOY_DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False #Decreases unnecessary output in terminal
 
     #SQLALCHEMY_DATABASE_URI
